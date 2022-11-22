f503b1b7675c51c99e3171025420b158b5b3a7ef
farzadjahandar <asdf!@#$A1371>
2022-11-22 16:59:45 +0300

    Switch between login/up clicking the link

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/RegisterActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/RegisterActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -22,5 +23,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView tv = findViewById(R.id.already_have_account);
+        String txt = "Already have an account? Sign in here";
+        SpannableString ss = new SpannableString(txt);
+        ClickableSpan cs = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.BLUE);
+            }
+        };
+        ss.setSpan(cs, 25, 37, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        tv.setText(ss);
+        tv.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
