4f335c2ea747444c4587d7f2c28c93721d8df728
farzadjahandar <asdf!@#$A1371>
2022-11-24 00:55:01 +0300

    sign out logic implemented

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/ProfileActivity.java
@@ -3,16 +3,21 @@ package edu.northeastern.numad22fa_team27.workout.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import edu.northeastern.numad22fa_team27.R;
+import edu.northeastern.numad22fa_team27.Util;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView usr_email;
     private FirebaseAuth user_auth;
+    private Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,25 @@ public class ProfileActivity extends AppCompatActivity {
 
         usr_email = findViewById(R.id.pusername);
         user_auth = FirebaseAuth.getInstance();
+        signOutBtn = findViewById(R.id.signOutBtn);
 
         loadUser();
+        setSignOutBtnClicked();
     }
 
     public void loadUser() {
         usr_email.setText(user_auth.getCurrentUser().getEmail());
+    }
+
+    public void setSignOutBtnClicked() {
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user_auth.signOut();
+                Toast.makeText(ProfileActivity.this, "Successfully signed out!", Toast.LENGTH_SHORT).show();
+                Util.openActivity(ProfileActivity.this, LoginActivity.class);
+            }
+        });
     }
 }
