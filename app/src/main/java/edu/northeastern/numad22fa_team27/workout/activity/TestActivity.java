69189b51255f9571dd4a989a213f4b2818cc2188
John Ciolfi <johnc2744@gmail.com>
2022-11-24 15:51:45 -0500

    shapeup: WIP implement groups

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/TestActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/TestActivity.java
@@ -16,10 +16,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.UUID;
 
 import edu.northeastern.numad22fa_team27.R;
 import edu.northeastern.numad22fa_team27.Util;
+import edu.northeastern.numad22fa_team27.workout.models.GroupDAO;
 import edu.northeastern.numad22fa_team27.workout.models.WorkoutCategory;
 
 public class TestActivity extends AppCompatActivity {
@@ -33,96 +35,99 @@ public class TestActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 
-    private void createGroup() {
-        final EditText groupName = new EditText(this);
-        groupName.setInputType(InputType.TYPE_CLASS_TEXT);
-
-        AlertDialog groupDialog = new AlertDialog.Builder(this)
-                .setTitle("Create New Group")
-                .setMessage("Enter the name for your group")
-                .setView(groupName)
-                .setPositiveButton("Create", null)
-                .setCancelable(false)
-                .create();
-
-        groupDialog.setOnShowListener(dialogInterface -> {
-            Button createButton = groupDialog.getButton(AlertDialog.BUTTON_POSITIVE);
-            createButton.setOnClickListener(view -> {
-                String providedUsername = groupName.getText().toString();
-                if (Util.stringIsNullOrEmpty(providedUsername)) {
-                    groupName.setError("Username can't be empty");
-                } else {
-                    // TODO: create group with UUID?
-
-                    groupDialog.dismiss();
-                }
-            });
-        });
-    }
+//    private void createGroup() {
+//        final EditText groupName = new EditText(this);
+//        groupName.setInputType(InputType.TYPE_CLASS_TEXT);
+//
+//        AlertDialog groupDialog = new AlertDialog.Builder(this)
+//                .setTitle("Create New Group")
+//                .setMessage("Enter the name for your group")
+//                .setView(groupName)
+//                .setPositiveButton("Create", null)
+//                .setCancelable(false)
+//                .create();
+//
+//        groupDialog.setOnShowListener(dialogInterface -> {
+//            Button createButton = groupDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+//            createButton.setOnClickListener(view -> {
+//                String providedGroupName = groupName.getText().toString();
+//                if (Util.stringIsNullOrEmpty(providedGroupName)) {
+//                    groupName.setError("Group name can't be empty");
+//                } else {
+//                    GroupDAO newGroup = new GroupDAO(providedGroupName, "SOME USER");
+//                    mDatabase
+//                            .child("group")
+//                            .child(String.valueOf(newGroup.getGroupID()))
+//                            .setValue(newGroup.pack());
+//                    groupDialog.dismiss();
+//                }
+//            });
+//        });
+//    }
 
     // TODO: think about pagination (.startAt/.startAfter/.limit)
-    private void findWorkoutsByCriteria(String workoutName, WorkoutCategory workoutCategory) {
-        if (Util.stringIsNullOrEmpty(workoutName) && workoutCategory == null) {
-            warnBadParam("findWorkoutsByCriteria");
-        } else if (Util.stringIsNullOrEmpty(workoutName)) {
-
-        } else if (workoutCategory == null) {
-
-        } else {
-
-        }
-    }
-
-    private void findUserByUsername(String username) {
-        if (Util.stringIsNullOrEmpty(username)) {
-            warnBadParam("findUserByUsername");
-            return;
-        }
-
-        mDatabase.child("users")
-                .startAt(username)
-                .endAt(username + '\uf8ff')
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) { }
-                });
-
-    }
-
-    private void findUserGroups(String username) {
-        if (Util.stringIsNullOrEmpty(username)) {
-            warnBadParam("findUserGroups");
-            return;
-        }
-
-        mDatabase.child("users")
-                .child("joinedGroups")
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
-    }
-
-    private void findGroupsByCriteria(String groupName) {
-        if (Util.stringIsNullOrEmpty(groupName)) {
-            warnBadParam("findGroupsByCriteria");
-            return;
-        }
-
-        // TODO: same substring method as find users by username
-    }
+//    private void findWorkoutsByCriteria(String workoutName, WorkoutCategory workoutCategory) {
+//        if (Util.stringIsNullOrEmpty(workoutName) && workoutCategory == null) {
+//            warnBadParam("findWorkoutsByCriteria");
+//        } else if (Util.stringIsNullOrEmpty(workoutName)) {
+//
+//        } else if (workoutCategory == null) {
+//
+//        } else {
+//
+//        }
+//    }
+//
+//    private void findUserByUsername(String username) {
+//        if (Util.stringIsNullOrEmpty(username)) {
+//            warnBadParam("findUserByUsername");
+//            return;
+//        }
+//
+//        mDatabase.child("users")
+//                .startAt(username)
+//                .endAt(username + '\uf8ff')
+//                .addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) { }
+//                });
+//
+//    }
+//
+//    private void findUserGroups(String username) {
+//        if (Util.stringIsNullOrEmpty(username)) {
+//            warnBadParam("findUserGroups");
+//            return;
+//        }
+//
+//        mDatabase.child("users")
+//                .child("joinedGroups")
+//                .addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                    }
+//                });
+//    }
+//
+//    private void findGroupsByCriteria(String groupName) {
+//        if (Util.stringIsNullOrEmpty(groupName)) {
+//            warnBadParam("findGroupsByCriteria");
+//            return;
+//        }
+//
+//        // TODO: same substring method as find users by username
+//    }
 
     private void warnBadParam(String methodName) {
         Log.w("Test", String.format("%s: fields were null", methodName));
---
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/models/GroupDAO.java
@@ -0,0 +1,105 @@
+package edu.northeastern.numad22fa_team27.workout.models;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import edu.northeastern.numad22fa_team27.Util;
+
+public class GroupDAO {
+    private UUID groupID;
+    private String name;
+    private Set<String> members;
+
+    public GroupDAO(String groupName, String creatorUsername) {
+        groupID = UUID.randomUUID();
+        this.name = groupName;
+        // new LinkedList<String>() {{  add(genres.getSelectedItem().toString()); }},
+        this.members = new HashSet<>() {{ add(creatorUsername); }};
+    }
+
+    // ---------- UUID Code ----------
+    /**
+     * Get ID for this group, returned as a string
+     * @return this group's ID
+     */
+    public UUID getGroupID() {
+        return this.groupID;
+    }
+
+    // ---------- Group Name Code ----------
+    /**
+     * Get the name for this group
+     * @return name of this group
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Change the name of this group
+     * @param newName new name for this group
+     * @return true if name was successfully changed, false otherwise
+     */
+    public boolean tryChangeName(String newName) {
+        if (Util.stringIsNullOrEmpty(newName)) {
+            return false;
+        }
+
+        this.name = newName;
+        return true;
+    }
+
+    // ---------- Group Members Code ----------
+    /**
+     * Get members in this group, returned as a list
+     * @return members' usernames in this group
+     */
+    public Set<String> getMembers() {
+        return new HashSet<>(this.members);
+    }
+
+    /**
+     * Add member to this group
+     * @param username member joining this group
+     * @return true if they were newly added to group, false otherwise
+     */
+    public boolean tryAddMember(String username) {
+        if (this.members.contains(username)) {
+            return false;
+        }
+
+        return this.members.add(username);
+    }
+
+    /**
+     * Remove member from this group
+     * @param username member leaving this group
+     * @return true if they're in this group and were removed, false otherwise.
+     */
+    public boolean tryRemoveMember(String username) {
+        if (!this.members.contains(username)) {
+            return false;
+        }
+
+        return this.members.remove(username);
+    }
+
+    // --------------------------------------------------------------------
+
+    public class Group {
+        private String name;
+        private List<String> members;
+
+        private Group(String name, List<String> members) {
+            this.name = name;
+            this.members = members;
+        }
+    }
+
+    public Group pack() {
+        return new Group(this.name, new ArrayList<>(this.members));
+    }
+}
