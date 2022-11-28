0485f548288b8d3640e3fdf13d697883a85e8f42
farzadjahandar <asdf!@#$A1371>
2022-11-28 17:03:48 +0300

    Edit profile pic basic code implemented

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/SettingsActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/SettingsActivity.java
@@ -1,16 +1,150 @@
 package edu.northeastern.numad22fa_team27.workout.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 import edu.northeastern.numad22fa_team27.R;
+import edu.northeastern.numad22fa_team27.Util;
+import edu.northeastern.numad22fa_team27.workout.models.User;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private Uri imageUri;
+    private Button saveBtn;
+    private static final int PICK_IMAGE = 1;
+    UploadTask uploadTask;
+    StorageReference storageReference;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference;
+    FirebaseFirestore  db = FirebaseFirestore.getInstance();
+    DocumentReference documentReference;
+    User user;
+    String currentUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        imageView = findViewById(R.id.profilePic);
+        saveBtn = findViewById(R.id.saveButton);
+        FirebaseUser fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert fbUser != null;
+        currentUID = fbUser.getUid();
+
+        documentReference = db.collection("user").document(currentUID);
+        storageReference = FirebaseStorage.getInstance().getReference("Profile Images");
+        databaseReference = database.getReference("Users");
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadData();
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == PICK_IMAGE || resultCode == RESULT_OK || data != null || data.getData() != null) {
+                imageUri = data.getData();
+                Picasso.get().load(imageUri).into(imageView);
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private String getImageExt(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadData() {
+        if (imageUri != null) {
+            final StorageReference reference = storageReference.child(System.currentTimeMillis() + "." + getImageExt(imageUri));
+            uploadTask = reference.putFile(imageUri);
+
+            Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return reference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+
+                        Map<String, String> profile = new HashMap<>();
+                        profile.put("url", downloadUri.toString());
+
+                        documentReference.set(profile).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(SettingsActivity.this, "Changes Saved!", Toast.LENGTH_SHORT).show();
+
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Util.openActivity(SettingsActivity.this, ProfileActivity.class);
+                                    }
+                                }, 2000);
+                            }
+                        });
+                    }
+                }
+            });
+        }
     }
 }
