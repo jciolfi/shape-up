3899d9cabea4a2cc96c316e6054f727c0e679943
farzadjahandar <asdf!@#$A1371>
2022-11-29 16:42:55 +0300

    Successfuly shows the user image for different users

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/ProfileActivity.java
@@ -1,14 +1,23 @@
 package edu.northeastern.numad22fa_team27.workout.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import edu.northeastern.numad22fa_team27.R;
 import edu.northeastern.numad22fa_team27.Util;
@@ -22,6 +31,7 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView usr_email;
     private FirebaseAuth user_auth;
     private Button signOutBtn, settingsBtn;
+    private ImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,7 @@ public class ProfileActivity extends AppCompatActivity {
         user_auth = FirebaseAuth.getInstance();
         signOutBtn = findViewById(R.id.signOutBtn);
         settingsBtn = findViewById(R.id.mySettings);
+        profilePic = findViewById(R.id.profilePic);
 
         loadUser();
         setSignOutBtnClicked();
@@ -62,6 +73,28 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
-    // TODO
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentID = user.getUid();
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference = firestore.collection("users").document(currentID);
+        reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.getResult().exists()) {
+                    String url = task.getResult().getString("profilePic");
+
+                    Picasso.get().load(url).into(profilePic);
+                } else {
+                    Toast.makeText(ProfileActivity.this, "Couldn't fetch the profile for the user", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+// TODO
     // Implement the logic when user presses back button.
 }
--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/SettingsActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/SettingsActivity.java
@@ -3,47 +3,31 @@ package edu.northeastern.numad22fa_team27.workout.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.MediaStore;
 import android.view.View;
-import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.common.collect.BiMap;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.annotation.Nullable;
 
 import edu.northeastern.numad22fa_team27.R;
 import edu.northeastern.numad22fa_team27.Util;
-import edu.northeastern.numad22fa_team27.workout.models.User;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -94,6 +78,7 @@ public class SettingsActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<Uri> task) {
                             if (task.isSuccessful()) {
                                 updateProfilePic(task.getResult().toString());
+                                Util.openActivity(SettingsActivity.this, ProfileActivity.class);
                             }
                         }
                     });
