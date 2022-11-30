8f6d8a48cb4ae6f256e72afeaaf42e28c4ca5f10
farzadjahandar <asdf!@#$A1371>
2022-11-30 14:46:20 +0300

    Change username is being reflected in both firebase auth and firestore

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/SettingsActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/SettingsActivity.java
@@ -10,13 +10,19 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
@@ -34,8 +40,12 @@ public class SettingsActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button saveBtn;
     private Button cancelBtn;
+    private EditText emailChange, passChange;
     private ProgressBar pb;
     private Uri imageUri;
+    private Boolean picSelected = false;
+    private Boolean profilePicUpdated = false;
+    private Boolean credsUpdated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +56,13 @@ public class SettingsActivity extends AppCompatActivity {
         saveBtn = findViewById(R.id.saveButton);
         pb = findViewById(R.id.save_progressbar);
         cancelBtn = findViewById(R.id.cancelButton);
+        emailChange = findViewById(R.id.editTextEmail);
+        passChange = findViewById(R.id.editTextPass);
 
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadImage();
+                saveChanges();
             }
         });
 
@@ -67,41 +79,102 @@ public class SettingsActivity extends AppCompatActivity {
                 Intent photoIntent = new Intent(Intent.ACTION_PICK);
                 photoIntent.setType("image/*");
                 startActivityForResult(photoIntent, 1);
+                picSelected = true;
             }
         });
 
     }
 
-    private void uploadImage() {
-        pb.setVisibility(View.VISIBLE);
-        FirebaseStorage.getInstance()
-                .getReference("images/" + FirebaseAuth.getInstance()
-                         .getCurrentUser().getEmail()).putFile(imageUri)
-                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                pb.setVisibility(View.INVISIBLE);
-                if(task.isSuccessful()) {
-                    task.getResult().getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+    private void updateCreds() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = db.collection("users")
+                .document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        if (user != null) {
+
+            String username = emailChange.getText().toString();
+
+            if (!username.isEmpty()) {
+                if (!username.equals(user.getEmail())) {
+                    user.updateEmail(username).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
-                        public void onComplete(@NonNull Task<Uri> task) {
+                        public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
-                                updateProfilePic(task.getResult().toString());
-                                Util.openActivity(SettingsActivity.this, ProfileActivity.class);
+                                credsUpdated = true;
+                                documentReference.update("username", username);
+                            }
+                            else {
+                                Toast.makeText(SettingsActivity.this, "Couldn't update the email!", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-                    Toast.makeText(SettingsActivity.this, "Changes Successfully Saved!", Toast.LENGTH_SHORT).show();
+                    credsUpdated = true;
                 } else {
-                    Toast.makeText(SettingsActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    credsUpdated = false;
+                    Toast.makeText(this, "New username cannot be same as the previous one!", Toast.LENGTH_SHORT).show();
                 }
+            } else {
+                credsUpdated = true;
             }
-        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+        }
 
-                    }
-                });
+
+    }
+
+    private void changeProfilePic() {
+        if (picSelected) {
+            FirebaseStorage.getInstance()
+                    .getReference("images/" + FirebaseAuth.getInstance().getCurrentUser().getEmail())
+                    .putFile(imageUri)
+                    .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            pb.setVisibility(View.INVISIBLE);
+                            if(task.isSuccessful()) {
+                                task.getResult().getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Uri> task) {
+                                        if (task.isSuccessful()) {
+                                            updateProfilePic(task.getResult().toString());
+                                        }
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(SettingsActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+            profilePicUpdated = true;
+        }
+        else {
+            profilePicUpdated = true;
+        }
+    }
+
+    private void saveChanges() {
+        pb.setVisibility(View.VISIBLE);
+        changeProfilePic();
+        updateCreds();
+        if (profilePicUpdated && credsUpdated) {
+            pb.setVisibility(View.INVISIBLE);
+            Toast.makeText(SettingsActivity.this, "Changes Successfully Saved!", Toast.LENGTH_SHORT).show();
+            Util.openActivity(SettingsActivity.this, ProfileActivity.class);
+        }
+
+        else {
+            if (profilePicUpdated) {
+                pb.setVisibility(View.INVISIBLE);
+                Toast.makeText(SettingsActivity.this, "Error in updating the username", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                pb.setVisibility(View.INVISIBLE);
+                Toast.makeText(SettingsActivity.this, "Error in updating the profile picture", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
     }
 
     private void updateProfilePic(String url) {
