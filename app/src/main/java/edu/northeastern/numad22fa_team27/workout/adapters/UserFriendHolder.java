2d3587ec22cbadc20db1b913d6cff3ec3ecffe20
bmontgom <montgomery.ben@northeastern.edu>
2022-12-02 14:25:53 -0500

    Add user friend screen

---
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/adapters/UserFriendHolder.java
@@ -0,0 +1,24 @@
+package edu.northeastern.numad22fa_team27.workout.adapters;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.northeastern.numad22fa_team27.R;
+
+public class UserFriendHolder  extends RecyclerView.ViewHolder {
+    public ImageView friendPicture;
+    public TextView friendName;
+
+    public UserFriendHolder(View view) {
+        super(view);
+        views(view);
+    }
+
+    private void views(View view) {
+        friendPicture = view.findViewById(R.id.profile_image);
+        friendName = view.findViewById(R.id.userFriendName);
+    }
+}
