341881d68b10da23034578d986f5fcfaf136ba80
John Ciolfi <johnc2744@gmail.com>
2022-12-02 22:26:59 -0500

    revert sending to search from main activity

--- app/src/main/java/edu/northeastern/numad22fa_team27/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 import edu.northeastern.numad22fa_team27.spotify.SpotifyActivity;
 import edu.northeastern.numad22fa_team27.sticker_messenger.FirebaseActivity;
+import edu.northeastern.numad22fa_team27.workout.activity.LoginActivity;
 import edu.northeastern.numad22fa_team27.workout.activity.SearchActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
         Button btnA8 = findViewById(R.id.btn_A8);
         btnA8.setOnClickListener(view -> Util.openActivity(this, FirebaseActivity.class));
         Button btnSU = findViewById(R.id.btn_proj);
-        btnSU.setOnClickListener(view -> Util.openActivity(this, SearchActivity.class));
+        btnSU.setOnClickListener(view -> Util.openActivity(this, LoginActivity.class));
     }
 }
