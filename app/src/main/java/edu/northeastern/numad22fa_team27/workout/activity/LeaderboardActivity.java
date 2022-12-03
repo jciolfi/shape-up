3492df40f78f30d361a8f8029ea3535f5d468b67
John Ciolfi <johnc2744@gmail.com>
2022-12-03 18:03:26 -0500

    create leaderboard activity + xml

---
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/LeaderboardActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad22fa_team27.workout.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.northeastern.numad22fa_team27.R;
+
+public class LeaderboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboard);
+    }
+}
