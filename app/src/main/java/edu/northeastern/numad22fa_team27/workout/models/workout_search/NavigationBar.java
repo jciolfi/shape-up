e923eaa8680b3322df10edf4b3792d894142cc64
fabigazi <fgaziano98@gmail.com>
2022-12-03 22:50:43 -0500

    toolbar is working well

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/models/workout_search/NavigationBar.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/models/workout_search/NavigationBar.java
@@ -1,6 +1,46 @@
 package edu.northeastern.numad22fa_team27.workout.models.workout_search;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import edu.northeastern.numad22fa_team27.R;
+import edu.northeastern.numad22fa_team27.spotify.SpotifyActivity;
+import edu.northeastern.numad22fa_team27.workout.activities.ui.main.viewpager.WorkoutListActivity;
+import edu.northeastern.numad22fa_team27.workout.activity.ProfileActivity;
+
 public class NavigationBar {
 
-    
+    public static NavigationBarView.OnItemSelectedListener setNavListener(Context context) {
+        NavigationBarView.OnItemSelectedListener navListener =
+                new BottomNavigationView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+
+                        switch (item.getItemId()) {
+                            case R.id.nav_leaderboard:
+                                Intent intent = new Intent(context, SpotifyActivity.class);
+                                context.startActivity(intent);
+                                break;
+                            case R.id.nav_profile:
+                                intent = new Intent(context, ProfileActivity.class);
+                                context.startActivity(intent);
+                                break;
+                            case R.id.nav_workout:
+                                intent = new Intent(context, WorkoutListActivity.class);
+                                context.startActivity(intent);
+                                break;
+                        }
+                        return false;
+                    }
+                };
+        return navListener;
+    }
 }
