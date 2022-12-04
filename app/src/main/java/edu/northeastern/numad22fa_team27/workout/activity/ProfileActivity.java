dd13d7dcfeaa4aee583be6378720defda83f0bf0
fabigazi <fgaziano98@gmail.com>
2022-12-03 22:27:44 -0500

    Finally working well

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/ProfileActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activity/ProfileActivity.java
@@ -2,8 +2,11 @@ package edu.northeastern.numad22fa_team27.workout.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +15,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -21,6 +26,9 @@ import com.squareup.picasso.Picasso;
 
 import edu.northeastern.numad22fa_team27.R;
 import edu.northeastern.numad22fa_team27.Util;
+import edu.northeastern.numad22fa_team27.spotify.SpotifyActivity;
+import edu.northeastern.numad22fa_team27.sticker_messenger.FirebaseActivity;
+import edu.northeastern.numad22fa_team27.workout.activities.ui.main.viewpager.WorkoutListActivity;
 import edu.northeastern.numad22fa_team27.workout.callbacks.FindGroupsCallback;
 import edu.northeastern.numad22fa_team27.workout.services.FirestoreService;
 
@@ -56,6 +64,9 @@ public class ProfileActivity extends AppCompatActivity {
         loadUser();
         setSignOutBtnClicked();
         settingsBtnClicked();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_toolbar);
+        bottomNav.setOnItemSelectedListener(navListener);
     }
 
     private void loadUser() {
@@ -120,4 +131,30 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener =
+            new BottomNavigationView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_leaderboard:
+                            Intent intent = new Intent(ProfileActivity.this, WorkoutListActivity.class);
+                            startActivity(intent);
+                            break;
+                        case R.id.nav_profile:
+                            intent = new Intent(ProfileActivity.this, ProfileActivity.class);
+                            startActivity(intent);
+                            break;
+                        case R.id.nav_workout:
+                            intent = new Intent(ProfileActivity.this, WorkoutListActivity.class);
+                            startActivity(intent);
+                            break;
+
+
+                    }
+                    return false;
+                }
+            };
 }
