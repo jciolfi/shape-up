5cdc2ede475bf71ece02526ccd1c0de79f5883bc
fabigazi <fgaziano98@gmail.com>
2022-12-06 10:28:22 -0500

    database progress

--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/activities/ui/main/viewpager/WorkoutListActivity.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/activities/ui/main/viewpager/WorkoutListActivity.java
@@ -1,28 +1,46 @@
 package edu.northeastern.numad22fa_team27.workout.activities.ui.main.viewpager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import edu.northeastern.numad22fa_team27.R;
+import edu.northeastern.numad22fa_team27.workout.activity.ProfileActivity;
 import edu.northeastern.numad22fa_team27.workout.models.workout_search.NavigationBar;
 
 public class WorkoutListActivity extends AppCompatActivity implements View.OnClickListener{
 
     private final List<WorkoutCard> workouts = new ArrayList<>();
+    private FirebaseAuth user_auth;
+    private List<String> title;
+    private String workoutTitle = "FlexibilityWorkout34";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_list);
+
+        user_auth = FirebaseAuth.getInstance();
 
         if (savedInstanceState == null) {
 
@@ -33,6 +51,7 @@ public class WorkoutListActivity extends AppCompatActivity implements View.OnCli
 
             }
         }
+
 
         workouts.add(new WorkoutCard(R.drawable.vinyl_vectorportal, "test 1", false));
         workouts.add(new WorkoutCard(R.drawable.baseball_vectorportal, "test 2", false));
@@ -51,6 +70,35 @@ public class WorkoutListActivity extends AppCompatActivity implements View.OnCli
         bottomNav.setSelectedItemId(R.id.nav_workout);
         bottomNav.setOnItemSelectedListener(NavigationBar.setNavListener(this));
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentID = user.getUid();
+        DocumentReference reference;
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+
+        reference = firestore.collection("workouts").document(currentID);
+        reference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.getResult().exists()) {
+                    Object workouts = task.getResult().get("workoutDescription", Object.class);//getString("profilePic");
+                    String username = task.getResult().getString("username");
+                    //usr_email.setText(username);
+                    if (true) {
+                        /*Picasso.get()
+                                .load(url)
+                                .resize(100, 100)
+                                .into(profilePic);*/
+                    }
+                } else {
+                    //Toast.makeText(ProfileActivity.this, "Couldn't fetch the profile for the user", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/edu/northeastern/numad22fa_team27/workout/fragments/GroupSearchFragment.java
+++ app/src/main/java/edu/northeastern/numad22fa_team27/workout/fragments/GroupSearchFragment.java
@@ -1,6 +1,5 @@
 package edu.northeastern.numad22fa_team27.workout.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
